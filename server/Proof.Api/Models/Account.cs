00fdacdfd42d9f18a805b503ae18c8857f88f104
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-07-09 18:41:26 -0400

    created Account entity

---
+++ server/Proof.Api/Models/Account.cs
@@ -0,0 +1,7 @@
+class Account
+{
+    public Guid Id { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
