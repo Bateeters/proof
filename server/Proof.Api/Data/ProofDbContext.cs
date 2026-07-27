84605f4c1aad7d10b8779b9721c993f674557a71
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-07-27 10:35:09 -0400

    added new properties to ProofDbContext.cs

--- server/Proof.Api/Data/ProofDbContext.cs
+++ server/Proof.Api/Data/ProofDbContext.cs
@@ -7,5 +7,9 @@ public class ProofDbContext : DbContext
 {
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Profile> Profiles { get; set; }
+    public DbSet<Cocktail> Cocktails { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<CocktailIngredient> CocktailIngredients { get; set; }
+    public DbSet<CocktailSeason> CocktailSeasons { get; set; }
     public ProofDbContext(DbContextOptions<ProofDbContext> options) : base(options) {}
 }
